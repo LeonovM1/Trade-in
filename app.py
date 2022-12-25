afc621867132c82d1e2c91fd99cf8bdfc1244b8d
LeonovM1 <mishaleonov00@mail.ru>
2022-12-25 17:36:16 +0300

    add page benifit, edit logo

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def about():
   return render_template("about.html")
 
 
+@app.route('/benefit')
+def benefit():
+  return render_template("benefit.html")
+
+
 if __name__ == "__main__":
   app.run(debug=True)
 
